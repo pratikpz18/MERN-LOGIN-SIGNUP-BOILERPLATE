9af83fa68286485ce198fe2f2b55dd0b4f5b9547
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-02-13 23:11:22 +0530

    done

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const user = require("./routes/user");
+const initiatemongoserver = require("./config/db");
+
+initiatemongoserver();
+
+const app = express();
+
+// PORT
+const PORT = process.env.PORT || 4000;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/'));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, Content-Type, Accept");
+  next();
+});
+
+// app.get("/", (req, res) => {
+//   res.json({ message: "API Working" });
+// });
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server Started at PORT ${PORT}`);
+});
+
+app.use(cors());
+app.use("/users/", user);
